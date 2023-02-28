efc39592cbf495cb38f99e038e8dceb2a78cd5dc
Fidel <eduardonaru@hotmail.com>
2023-02-27 22:32:51 -0600

    Pasando codigo de log in

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,28 @@
 export default function SignUp() {
     return (
         <>
-            <div>
-                <h1>Signup</h1>
-            </div>
+        <main>
+            <header>
+          <div className="container-h">
+              <div className='navegacion'>
+                <div className="nav-l">
+                    <Link href="/pages/index.js">
+                        <Image src={logo} alt="Tec Overflow" width={100} height={100} />
+                    </Link>
+                    <h1 className="titulo">Tec Overflow</h1>
+                </div>
+                <div className="nav">
+                    <div className="link">
+                        <Link href="/login" className='active'>Log in</Link>
+                    </div>
+                    <div className="link">
+                        <Link href="#">Sign up</Link>
+                    </div>
+                </div>
+              </div>
+          </div>
+        </header>
+        </main>
         </>
     )
 };
