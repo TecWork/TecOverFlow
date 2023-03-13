1158a0aa7039117919b98d556e72634f2daf94f9
Fidel <eduardonaru@hotmail.com>
2023-03-13 15:29:32 -0600

    Terminando pantalla sin aguilas

--- pages/cam_contraCorreo.js
+++ pages/cam_contraCorreo.js
@@ -2,7 +2,7 @@ import logo from '/public/logo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
 import biglogo from '/public/biglogo.svg'
-import styles from "@/styles/cambiarcontracorreo.css"
+import styles from '@/styles/cam.contra.correo.module.css'
 
 export default function SignUp() {
     return (
@@ -18,10 +18,10 @@ export default function SignUp() {
               </Link>
               <ul className='nav-link'>
                 <li className='nav-item'>
-                  <Link className='' href='/'>Log In</Link>
+                  <Link className={styles.active} href='/'>Log In</Link>
                 </li>
                 <li className='nav-item'>
-                  <Link className={styles.active} href='/'>Sign Up</Link>
+                  <Link className='' href='/'>Sign Up</Link>
                 </li>
               </ul>
             </div>
@@ -33,8 +33,8 @@ export default function SignUp() {
         <div className={styles.contenedor_principal}>
           {/* <Image src={biglogo} alt="Tec Overflow" height={700} className={styles.container_img_l}/> */}
           <div className={styles.container}>
-            <h1 className={styles.titulo}>Registrarte</h1>
-            <div className={styles.datos}>
+            <h1 className={styles.titulo}>Recuperar contraseña</h1>
+            {/*<div className={styles.datos}>
                 <div className={styles.datos_bloque}>
                   <label className={styles.label}>Nombre</label>
                   <input type="text" name="nombre" className={styles.inputnombre}/>
@@ -43,22 +43,26 @@ export default function SignUp() {
                   <label className={styles.label}>Apellido</label>
                   <input type="text" name="apellido" className={styles.inputnombre}/>
                 </div>
-              </div>
+            </div>*/}
             <div className={styles.form}>
               <label className={styles.label}>Correo electrónico</label>
               <input type="email" name="email" placeholder="Ingresa tu email" className={styles.input}/>
+              {/*}
               <label className={styles.label}>Contraseña</label>
               <input type="password" name="password" placeholder="Ingresa tu contraseña" className={styles.input} />
               <label className={styles.label}>Confirmar contraseña</label>
               <input type="password" name="confirmpassword" placeholder="Confirmar contraseña" className={styles.input} />
+              */}
               <br/>
+              {/*}
               <div className={styles.check}>
                 <input type="checkbox" id="privacidad" name="terminos" className={styles.check}/>
                 <label>Aceptar los terminos y privacidad</label>
               </div>
+            */}
             </div>
             <br/>
-            <button className={styles.button}>Iniciar sesión</button>
+            <button className={styles.button}>Enviar</button>
             <label className={styles.no_cuenta}>¿No tienes una cuenta?<Link href="#" className={styles.registrate}> Registrate</Link></label>
           </div>
          {/*  <Image src={biglogo} alt="Tec Overflow" height={700} className="container-img-l"/> */}
