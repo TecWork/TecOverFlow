97673d42f5df199fa383fcf385d77d2f8fa44150
Eduardoespl <eduardoesp3317@gmail.com>
2023-03-14 23:05:33 -0600

    inicio validacion ventana login

--- pages/login.js
+++ pages/login.js
@@ -37,51 +37,48 @@ export default function Login() {
                         <h1 className={styles.h1}>Iniciar sesión</h1>
                         <br/>
                         <div className={styles.form}>
-                            <label className={styles.label}>Correo electrónico</label>
-                            <input id='input1' type="email" name="email" placeholder="Ingresa tu email" className={styles.input}/>
+                            <label className={styles.label} >Correo electrónico</label>
+                            <input id='input1' type="email" name="email" placeholder="Ingresa tu email" className={styles.input} required/>
                             <br/>
                             <br/>
                             <label className={styles.label}>Contraseña</label>
-                            <input id='input2' type="password" name="password" placeholder="Ingresa tu contraseña" className={styles.input}/>
+                            <input id='input2' type="password" name="password" placeholder="Ingresa tu contraseña" className={styles.input} required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[ !@#$%^&*_=+-]).{8,30}$" title="Al menos 6 caracteres, máximo 12, debe contener una letra mayúscula, una minuscula, un número y no puede contener caracteres especiales"/>
                         </div>
                         <Link href="#" className={styles.olvide_link}>¿Olvidaste tu contraseña?</Link>
                         <br/>
                         <br/>
-                        <button id="button" className={styles.button}>Iniciar sesión</button>
-                        <label className={styles.no_cuenta}>¿No tienes una cuenta?<Link href="#" className={styles.registrate}> Registrate</Link></label>
+                        <button id="button" className={styles.button} href='/'>Iniciar sesión</button>
+                        <label className={styles.no_cuenta}>¿No tienes una cuenta?<Link href="/" className={styles.registrate}> Registrate</Link></label>
                     </div>
                     <Image id="imagen2" src={biglogo} alt="Tec Overflow" height={600} className={styles.imagen_rotada}/>
                 </div>
                 <Script id="script">
                     {`
-                        const button = document.getElementById("button");
-                        const input1 = document.getElementById("input1");
-                        const input2 = document.getElementById("input2");
-
-                        button.addEventListener("click", () => {
-                            if (input1.value === "") {
-                                input1.style.border = "1px solid red";
-                                alert("Los campos no pueden estar vacios");
-                                if (input2.value === "") {
-                                    input2.style.border = "1px solid red";
-                                    alert("Los campos no pueden estar vacios");
-                                } else {
-                                    input2.style.border = "1px solid #000000";
-                                }
+                        const email = document.querySelector("#input1");
+                        const password = document.querySelector("#input2");
+                        const button = document.querySelector("#button");
+                        
+                        email.addEventListener('blur', () => {
+                            if (email.value === "" ){
+                                email.style.border = "1px solid red";
                             }else{
-                                input1.style.border = "1px solid #000000";
-                                if (input2.value === "") {
-                                    input2.style.border = "1px solid red";
-                                    alert("Los campos no pueden estar vacios");
-                                } else {
-                                    input2.style.border = "1px solid #000000";
-                                    console.log(input2.value);
-                                    console.log(input1.value);
-                                }
-                            input1.value = "";
-                            input2.value = "";
+                                email.style.border = "1px solid black";
                             }
                         });
+
+                        password.addEventListener('blur', () => {
+                            if (password.value === ""){
+                                password.style.border = "1px solid red";
+                            }else{
+                                password.style.border = "1px solid black";
+                            }
+                        });
+
+                        button.addEventListener('click', () => {
+                            console.log(email.validity.valid);
+                            console.log(password.validity.valid);
+                        });
+                        
                     `}
                 </Script>
             </main>
