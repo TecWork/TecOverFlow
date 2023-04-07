baab808eabf875f7d5b095caebb1462558be0ac0
Eduardoespl <eduardoesp3317@gmail.com>
2023-04-07 00:15:38 -0600

    Correción validar formulario login + toast succes

--- pages/login.js
+++ pages/login.js
@@ -37,25 +37,26 @@ export default function Login() {
                     <div className={styles.container}> {/* Este div contiene todo lo que esta dentro de la pantalla */}
                         <h1 className={styles.h1}>Iniciar sesión</h1> {/* Este es el titulo de la pantalla */}
                         <br/>
-                        <div className={styles.form}> {/* Este div contiene el formulario */}
+                        <form className={styles.form}> {/* Este div contiene el formulario */}
                             <label className={styles.label} >Correo electrónico</label> {/* Este es el label del input de correo electronico */}
                             <input id='input1' type="email" name="email" placeholder="Ingresa tu email" className={styles.input} required/> {/* Este es el input de correo electronico */}
                             <br/>
                             <br/>
                             <label className={styles.label}>Contraseña</label> {/* Este es el label del input de contraseña */}
                             <input id='input2' type="password" name="password" placeholder="Ingresa tu contraseña" className={styles.input} required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[ !@#$%^&*_=+-]).{8,30}$" title="Al menos 6 caracteres, máximo 12, debe contener una letra mayúscula, una minuscula, un número y no puede contener caracteres especiales"/> {/* Este es el input de contraseña, el "pattern" son las condicionales que debe de cumplir para que sea valida, y el "title" es el mensaje que aparecera al poner el cursor sobre el input */}
-                        </div>
-                        <Link href="/cam_contraCorreo" className={styles.olvide_link}>¿Olvidaste tu contraseña?</Link> {/* Este link nos dirije a la pantalla de olvide mi contraseña */}
-                        <br/>
-                        <br/>
-                        <Link href="#" id="button" className={styles.button}>Iniciar sesión</Link> {/* Verificar como hacer que te dirija a la pagina solo cuando se cumplen los parametros */}
-                        <label className={styles.no_cuenta}>¿No tienes una cuenta?<Link href="/" className={styles.registrate}> Registrate</Link></label> {/* Este label nos dirije a la pantalla de Sign up */}
+                            <Link href="/cam_contraCorreo" className={styles.olvide_link}>¿Olvidaste tu contraseña?</Link> {/* Este link nos dirije a la pantalla de olvide mi contraseña */}
+                            <br/>
+                            <br/>
+                            <button type="submit" id="button" className={styles.button}>Iniciar sesión</button> {/* Verificar como hacer que te dirija a la pagina solo cuando se cumplen los parametros */}
+                            {/* <Link href="#" id="check" className={styles.check}></Link> */}
+                            <label className={styles.no_cuenta}>¿No tienes una cuenta?<Link href="/" className={styles.registrate}> Registrate</Link></label> {/* Este label nos dirije a la pantalla de Sign up */}
+                        </form>
                         
-                        {/* Insertar un toast y darle el display de none */}
-                    <div id="toast" className={styles.toast}>
+                        {/* Toast de error */}
+                        <div id="toaste" className={styles.toaste}>
                             <div id="toast-success" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-red-700" role="alert">
                                 <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-red-100 rounded-lg dark:bg-red-700 dark:text-green-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /> </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /> </svg>
                                     <span class="sr-only">x-mark icon</span>
                                 </div>
                                 <div className={styles.toast_text}>
@@ -64,17 +65,35 @@ export default function Login() {
                                 </div>
                             </div>
                         </div>
-                        {/* Aqui termina el toast */}
+                        {/* Aqui termina el toast de error */}
+
+                        {/* Toast succes */}
+                        <div id="toasts" className={styles.toasts}>
+                            <div id="toast-success" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-green-700" role="alert">
+                                <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-700 dark:text-green-200">
+                                    <svg fill="none" stroke="white" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+                                    </svg>
+                                    <span class="sr-only">CheckIcon </span>
+                                </div>
+                                <div className={styles.toast_text}>
+                                    <div className={styles.texto_tipo}>Correcto</div>
+                                    <div className={styles.texto_desc}>Acceso correcto</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <Image id="imagen2" src={biglogo} alt="Tec Overflow" height={600} className={styles.imagen_rotada}/> {/* Esta imagen es la que aparece a los costados de la pantalla pero rotada */}
                 </div>
                 <Script id="script">
                     {`
+                        const form = document.querySelector("form");
                         const email = document.querySelector("#input1"); 
                         const password = document.querySelector("#input2");
                         const button = document.querySelector("#button");
-                        const toast = document.querySelector("#toast");
+                        const toaste = document.querySelector("#toaste");
+                        const toasts = document.querySelector("#toasts");
 
                         email.addEventListener('blur', () => {
                             if (email.value === "" ){
@@ -92,18 +111,35 @@ export default function Login() {
                             }
                         });
 
+                        form.addEventListener('submit', (e) => {
+                            e.preventDefault();
+
+                            if (email.validity.valid && password.validity.valid){
+                                console.log("hola");
+                                setTimeout(function(){window.location.href = "/";}, 3000);
+                                toasts.style.display = "block";
+                                setTimeout(function(){ toasts.style.display = "none"; }, 3000);
+
+                            }else{
+                                password.style.border = "1px solid red";
+                                email.style.border = "1px solid red";
+                                toaste.style.display = "block";
+                                setTimeout(function(){ toast.style.display = "none"; }, 3000);
+                            }
+                        });
+
+
                         button.addEventListener('click', () => {
                             console.log(email.validity.valid);
                             console.log(password.validity.valid);
                             if (email.validity.valid && password.validity.valid){
-                                button.location.href="/"
+                                console.log("hola");
                             }else{
-                                password.style.border = "1px solid red";
-                                email.style.border = "1px solid red";
                                 toast.style.display = "block";
                                 setTimeout(function(){ toast.style.display = "none"; }, 3000);
                             }
                         });
+
                     `}
                 </Script>
             </main>
